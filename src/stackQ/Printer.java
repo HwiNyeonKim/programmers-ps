ac602330d35f944af159fedec4a86b0c2448a2bb
z2b23173us <z2b23173us@gmail.com>
2021-03-23 20:07:20 +0900

    AL-6 [Stack and Queue] 프린터 change ArrayList -> ArrayDequeue, and remove redundant codes

--- src/stackQ/Printer.java
+++ src/stackQ/Printer.java
@@ -1,22 +1,21 @@
 package stackQ;
 
 import java.util.ArrayDeque;
-import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Printer {
     public int solution(int[] priorities, int location) {
-        // 1. Very naive way will be O(n^2)
-        ArrayList<Document> queue = new ArrayList<>();
+        ArrayDeque<Document> queue = new ArrayDeque<>();
 
         for (int i = 0; i < priorities.length; i++) {
-            queue.add(new Document(i == location, priorities[i]));
+            queue.offer(new Document(i == location, priorities[i]));
         }
 
         int count = 0;
         while (!queue.isEmpty()) {
-            Document doc = queue.remove(0);
+            Document doc = queue.poll();
             if (!isPrintable(doc, queue)) {
-                queue.add(doc);
+                queue.offer(doc);
                 continue;
             }
 
@@ -28,10 +27,13 @@ public class Printer {
         return -1;
     }
 
-    boolean isPrintable(Document doc, ArrayList<Document> queue) {
+    boolean isPrintable(Document doc, ArrayDeque<Document> queue) {
         int docPriority = doc.getPriority();
-        for (int i = 0; i < queue.size(); i++) {
-            if (docPriority < queue.get(i).getPriority()) {
+
+        Iterator<Document> iter = queue.iterator();
+
+        while (iter.hasNext()) {
+            if (docPriority < iter.next().getPriority()) {
                 return false;
             }
         }
@@ -39,9 +41,9 @@ public class Printer {
     }
 }
 
-class Document implements Comparable {
-    private boolean isMine;
-    private int priority;
+class Document {
+    private final boolean isMine;
+    private final int priority;
 
     Document(boolean isMine, int priority) {
         this.isMine = isMine;
@@ -54,12 +56,5 @@ class Document implements Comparable {
 
     public int getPriority() {
         return this.priority;
-    }
-
-    @Override
-    public int compareTo(Object o) {
-        Document doc = (Document)o;
-        // 순서를 바꾸어서 일반적인 ascending order로 sorting 할 때 priority가 높은 것이 앞으로 오도록
-        return doc.getPriority() - this.priority;
     }
 }
