9863d0ae9937b9c05fb5ac1b78aad96208141f9f
HwiNyeon Kim <z2b23173us@gmail.com>
2021-04-05 16:24:26 +0900

    [Sorting] K번째 수 - Sol.2 Bubble Sort

--- src/sorting/KthNumber.java
+++ src/sorting/KthNumber.java
@@ -9,9 +9,16 @@ public class KthNumber {
         for (int i = 0; i < commands.length; i++) {
             int[] command = commands[i];
 
+            // cut array
             int[] target = Arrays.copyOfRange(array, command[0] - 1, command[1]);
-            sort(target);
 
+            // sort
+            // Solution 0
+            // sort(target);
+            // Solution 1
+            bubbleSort(target);
+
+            // record answer
             answer[i] = target[command[2] - 1];
         }
 
@@ -21,5 +28,22 @@ public class KthNumber {
     // Solution 0: 내장 함수 사용
     public static void sort(int[] arr) {
         Arrays.sort(arr);
+    }
+
+    // Solution 1: Bubble Sort
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    swap(arr, i, j);
+                }
+            }
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
--- src/sorting/KthNumberTest.java
+++ src/sorting/KthNumberTest.java
@@ -10,50 +10,27 @@ class KthNumberTest {
     @Test
     void solution() {
         assertArrayEquals(
-                obj.solution(
-                        new int[] {1, 5, 2, 6, 3, 7, 4},
-                        new int[][] {
-                                {2, 5, 3},
-                                {4, 4, 1},
-                                {1, 7, 3}
-                        }
-                ),
-                    new int[] {5, 6, 3}
+            obj.solution(
+                new int[] {1, 5, 2, 6, 3, 7, 4},
+                new int[][] {
+                    {2, 5, 3},
+                    {4, 4, 1},
+                    {1, 7, 3}
+                }
+            ), new int[] {5, 6, 3}
+        );
+    }
+
+    @Test
+    void checker() {
+        assertArrayEquals(
+            obj.solution(
+                new int[] {9, 8, 7, 1, 2, 3, 4, 5, 6},
+                new int[][] {
+                    {1, 9, 3},
+                    {2, 8, 3}
+                }
+            ), new int[] {3, 3}
         );
     }
 }
-
-
-
-    // Solution 1: Quick Sort
-    public static void quickSort(int[] arr, int left, int right) {
-        if (left >= right) {
-            return;
-        }
-
-        int pivotValue = arr[right];
-        int i = 0;
-        int j = right - 1;
-        while (i <= j) {
-            while (arr[i] <= pivotValue && i < j) {
-                // 현재 선택된 pivot 보다 큰 값들은 최종적으로 pivot의 우측으로 옮겨져야 함
-                i++;
-            }
-            while (arr[j] > pivotValue && i < j) {
-                // 현재 선택된 pivot 보다 작은 값들은 최종적으로 pivot의 좌측으로 옮겨져야 함.
-                j--;
-            }
-            swap(arr, i, j);
-        }
-        // pivot의 좌, 우측 분할 완료
-        swap(arr, i, right); // 피벗 위치 교환
-
-        quickSort(arr, left, i - 1);
-        quickSort(arr, i + 1, right);
-    }
-
-    public static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
