dc6a78f561daec1de277c18ee3bc545c937a7063
HwiNyeon <z2b23173us@gmail.com>
2021-04-20 16:41:53 +0900

    [Hash] 완주하지 못한 선수 - 정렬 이용 방법 추가

--- src/hash/PlayersNotFinished.java
+++ src/hash/PlayersNotFinished.java
@@ -1,10 +1,22 @@
 package hash;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class PlayersNotFinished {
     public String solution(String[] participant, String[] completion) {
+        // Solution 1. 정렬 이용
+        // Arrays.sort(participant);
+        // Arrays.sort(completion);
+        // for (int i = 0; i < completion.length; i++) {
+        //     if (!participant[i].equals(completion[i])) {
+        //         return participant[i];
+        //     }
+        // }
+        // return participant[participant.length - 1];
+
+        // Solution 2. Map 이용
         Map<String, Integer> participants = new HashMap<>(); // 이름 : 해당 이름을 가진 참가자 수
 
         // 1. 참가자 인원 업데이트
