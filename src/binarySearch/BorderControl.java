3fbff8657c277bf1f9374bce14422c1622cc126d
HwiNyeon Kim <z2b23173us@gmail.com>
2021-05-17 17:41:22 +0900

    [BinarySearch] 입국심사

---
+++ src/binarySearch/BorderControl.java
@@ -0,0 +1,34 @@
+package binarySearch;
+
+import java.util.Arrays;
+
+public class BorderControl {
+    public long solution(int n, int[] times) {
+        Arrays.sort(times);
+        long minimum = 1;
+        long maximum = (long) times[times.length - 1] * n;
+        long mid = (minimum + maximum) / 2;
+
+        long answer = count(mid, times);
+        while (minimum < maximum) {
+            if (answer >= n) {
+                maximum = mid;
+            } else {
+                minimum = mid + 1;
+            }
+            mid = (minimum + maximum) / 2;
+            answer = count(mid, times);
+        }
+
+        return mid;
+    }
+
+    private long count(long t, int[] times) {
+        // 주어진 임의의 시간 t에 대해 입국 심사 처리가 가능한 사람의 수를 반환
+        long answer = 0;
+        for (int time : times) {
+            answer += (t / time);
+        }
+        return answer;
+    }
+}
