3cb917773a16eeea701d4fa31a8eb8e38be9f944
HwiNyeon Kim <z2b23173us@gmail.com>
2021-05-19 15:35:15 +0900

    [Dynamic Programming] N으로 표현

---
+++ src/DynamicProgramming/RepresentWithNTest.java
@@ -0,0 +1,19 @@
+package DynamicProgramming;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RepresentWithNTest {
+    RepresentWithN obj = new RepresentWithN();
+
+    @Test
+    void solution1() {
+        assertEquals(4, obj.solution(5, 12));
+    }
+
+    @Test
+    void solution2() {
+        assertEquals(3, obj.solution(2, 11));
+    }
+}
