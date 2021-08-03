886a202a6e90966564c8dc2350af8c47020e8240
HwiNyeon Kim <z2b23173us@gmail.com>
2021-08-03 18:37:41 +0900

    [연습문제] 배달

---
+++ src/exercise/Delivery.java
@@ -0,0 +1,58 @@
+package exercise;
+
+import java.util.*;
+
+public class Delivery {
+    final int inf = Integer.MAX_VALUE;
+
+    public int solution(int N, int[][] roads, int K) {
+        int[][] towns = new int[N][N];
+        for (int[] town : towns) {
+            Arrays.fill(town, inf);
+        }
+        towns[0][0] = 0; // 1번 마을은 무조건 배달 가능!
+
+        for (int[] road : roads) {
+            towns[road[0] - 1][road[1] - 1] = Math.min(towns[road[0] - 1][road[1] - 1], road[2]);
+            towns[road[1] - 1][road[0] - 1] = Math.min(towns[road[1] - 1][road[0] - 1], road[2]);
+        }
+
+        Set<Integer> visited = new HashSet<>();
+        visited.add(0); // starting point
+
+        while (visited.size() < N) {
+            int visitTarget = getNextVisit(towns[0], visited);
+            visited.add(visitTarget);
+
+            int[] visitable = towns[visitTarget];
+            for (int i = 0; i < visitable.length; i++) {
+                if (visitable[i] < inf) {
+                    towns[0][i] = Math.min(towns[0][i], towns[0][visitTarget] + visitable[i]);
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            if (towns[0][i] <= K) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private int getNextVisit(int[] distances, Set<Integer> visited) {
+        int idx = 0;
+        int min = inf;
+
+        for (int i = 0; i < distances.length; i++) {
+            if (distances[i] < min && !visited.contains(i)) {
+                idx = i;
+                min = distances[i];
+            }
+        }
+
+        return idx;
+    }
+}
