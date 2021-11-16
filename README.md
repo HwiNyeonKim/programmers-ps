d26506150b541d1e95cca3e84d29c4f9b3701245
HwiNyeon <z2b23173us@gmail.com>
2021-11-17 05:00:31 +0900

    [BruteForce] 소수 찾기 - 코드 개선

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 | --- | --- | --- | --- | --- | --- |
 | 입국심사 | Binary Search | 3 | Solved | [링크](https://praetoriani.tistory.com/26) | - |
 | 카펫 | Brute Force | 2 | Solved | [링크](https://praetoriani.tistory.com/9) | 2021.11.16 |
+| 소수 찾기 | Brute Force | 2 | Solved | [링크](https://programmers.co.kr/learn/courses/30/lessons/42839) | 2021.11.17 |
 | 작성중 ...| 
--- src/bruteforce/FindPrimeNumber.java
+++ src/bruteforce/FindPrimeNumber.java
@@ -1,52 +1,24 @@
 package bruteforce;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FindPrimeNumber {
-    String[] result;
-    int idx = 0;
-
     public int solution(String numbers) {
-        // 1. 가능한 모든 조합 찾기
-        int lengthOfCases = 0;
-        int lengthOfNumbers = numbers.length();
-        for (int i = lengthOfNumbers; i > 0; i--) {
-            int count = 1;
-            for (int j = lengthOfNumbers; j > lengthOfNumbers - i; j--) {
-                count *= j;
-            }
-            lengthOfCases += count;
+        char[] nums = numbers.toCharArray();
+        // 1장을 사용해서 만들 수 있는 숫자부터 n 장을 사용해서 만들 수 있는 숫자까지 모두 고려
+        Set<Integer> createdNums = new HashSet<Integer>();
+        for(int i = 1; i <= nums.length; i++) {
+            permutation(nums, 0, i, createdNums);
         }
 
+        boolean[] primeTable = getPrimeNumberTable(Collections.max(createdNums));
 
-        this.result = new String[lengthOfCases];
-        for (int i = 1; i <= numbers.length(); i++) {
-            this.permutation(numbers.toCharArray(), 0, i);
-        }
-
-        // 2. 맨 앞에 0이 들어간 숫자 (e.g. 011 -> 11) 수정 및 같은 숫자 제거
-        Set<Integer> candidates = new HashSet<>();
-        for (String str : result) {
-            candidates.add(Integer.parseInt(str));
-        }
-
-        // 3. 만들어진 숫자들 중 최댓값 찾기 for Sieve of Eratosthenes
-        // ref: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
-        int max = 0;
-        for (int current : candidates) {
-            if (current > max) {
-                max = current;
-            }
-        }
-
-        // 4. 소수를 찾기 위한 표 만들기
-        boolean[] primeList = new boolean[max + 1];
-        this.buildPrimeList(primeList);
-
-        // 5. 소수 찾기 & 카운팅
         int count = 0;
-        for (int num : candidates) {
-            if (isPrime(num, primeList)) {
+        for (Integer createdNum : createdNums) {
+            if (primeTable[createdNum]) {
                 count++;
             }
         }
@@ -54,45 +26,19 @@ public class FindPrimeNumber {
         return count;
     }
 
-    // Helper function for finding prime numbers
-    private void buildPrimeList(boolean[] primeList) {
-        Arrays.fill(primeList, true);
-        primeList[0] = false;
-        primeList[1] = false;
-
-        // 에라토스테네스의 체
-        int i = 2;
-        while (i * i <= primeList.length - 1) {
-            if (primeList[i]) {
-                int j = i * 2;
-                while (j <= primeList.length - 1) {
-                    primeList[j] = false;
-                    j += i;
-                }
-            }
-            i++;
-        }
-    }
-
-    private boolean isPrime(int num, boolean[] primeList) {
-        return primeList[num];
-    }
-
-    // Heap's algorithm
-    // ref: https://en.wikipedia.org/wiki/Heap%27s_algorithm
-    private void permutation(char[] chars, int depth, int r) {
+    private void permutation(char[] chars, int depth, int r, Set<Integer> resultContainer) {
         if (depth == r) {
             StringBuilder answer = new StringBuilder();
             for (int i = 0; i < r; i++) {
                 answer.append(chars[i]);
             }
-            this.result[idx++] = answer.toString();
+            resultContainer.add(Integer.valueOf(answer.toString()));
             return;
         }
 
         for (int i = depth; i < chars.length; i++) {
             this.swap(chars, i, depth);
-            this.permutation(chars, depth + 1, r);
+            this.permutation(chars, depth + 1, r, resultContainer);
             this.swap(chars, i, depth);
         }
     }
@@ -101,5 +47,23 @@ public class FindPrimeNumber {
         char temp = chars[i];
         chars[i] = chars[j];
         chars[j] = temp;
+    }
+
+    private boolean[] getPrimeNumberTable(int upperBound) {
+        boolean[] primes = new boolean[upperBound + 1];
+        Arrays.fill(primes, true);
+
+        primes[0] = false;
+        primes[1] = false;
+
+        for (int i = 2; i * i <= upperBound; i++) {
+            if (primes[i]) {
+                for (int j = i * 2; j <= upperBound; j += i) {
+                    primes[j] = false;
+                }
+            }
+        }
+
+        return primes;
     }
 }
