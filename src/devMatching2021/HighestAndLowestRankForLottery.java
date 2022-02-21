d56b9990dea1f7189a016ef49a7f566151974986
HwiNyeon Kim <z2b23173us@gmail.com>
2022-02-21 17:12:45 +0900

    [2021 Dev-Matching 백엔드] 로또의 최고 순위와 최저 순위

---
+++ src/devMatching2021/HighestAndLowestRankForLottery.java
@@ -0,0 +1,27 @@
+package devMatching2021;
+
+import java.util.Arrays;
+
+// 문제 : https://programmers.co.kr/learn/courses/30/lessons/77484
+// 2021 Dev-Matching 웹 백엔드 개발자 : 로또의 최고 순위와 최저 순위
+public class HighestAndLowestRankForLottery {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        // 알고있는 번호만 남기기
+        lottos = Arrays.stream(lottos)
+                .filter(number -> number != 0)
+                .toArray();
+
+        // 맞춘 번호와 모르는 번호 갯수 계산
+        int unknownCount = 6 - lottos.length;
+        // 맞춘 갯수가 0개나 1개나 동일한 6등
+        long matchedCount = Math.max(Arrays.stream(lottos)
+                .filter(number ->
+                        Arrays.stream(win_nums)
+                                .anyMatch(i -> i == number)
+                )
+                .count(), 1);
+
+        int highest = Math.max(1, (int)(7 - (matchedCount + unknownCount)));
+        return new int[] {highest, 7 - (int) matchedCount};
+    }
+}
