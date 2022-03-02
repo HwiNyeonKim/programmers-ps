9fd984dd56eb411e4f5c8f01ed2e905a8da3dbd0
HwiNyeon Kim <z2b23173us@gmail.com>
2022-03-02 17:14:44 +0900

    [Refactor] - [DP] N으로 표현
    불필요하게 list.get() 연산을 여러 번 수행하던 부분 제거

--- src/dynamicProgramming/RepresentWithN.java
+++ src/dynamicProgramming/RepresentWithN.java
@@ -7,45 +7,40 @@ import java.util.Set;
 
 public class RepresentWithN {
     public int solution(int N, int number) {
-        List<Set<Integer>> list = new ArrayList<>();
+        List<Set<Integer>> result = new ArrayList<>();
+
         for (int i = 0 ; i < 8; i++) {
-            list.add(new HashSet<>());
+            result.add(new HashSet<>());
         }
-        list.get(0).add(N); // N을 1개만 사용할 경우, N 자신만 표현 가능
+        result.get(0).add(N); // N을 1개만 사용할 경우, N 자신만 표현 가능
 
         for (int i = 0; i < 8; i++) {
+            Set<Integer> currentResult = result.get(i);
             // 1. N을 (i + 1)번 이어붙인 경우
-            list.get(i).add(Integer.parseInt(stringRepeat(N, i + 1)));
+            currentResult.add(Integer.parseInt(String.valueOf(N).repeat(i + 1)));
 
             // 2. 이전 단계의 결과를 서로 사칙연산한 결과
             // [N을 1개 사용한 경우] union [(i + 1) - 1개 사용한 경우]
             // [N을 2개 사용한 경우] union [(i + 1) - 2개 사용한 경우]
             // ...
             for (int j = 0; j < i; j++) {
-                for (int case1 : list.get(j)) {
-                    for (int case2 : list.get(i - 1 - j)) {
-                        list.get(i).add(case1 + case2);
-                        list.get(i).add(case1 - case2);
-                        list.get(i).add(case1 * case2);
+                for (int case1 : result.get(j)) {
+                    for (int case2 : result.get(i - 1 - j)) {
+                        currentResult.add(case1 + case2);
+                        currentResult.add(case1 - case2);
+                        currentResult.add(case1 * case2);
                         if (case2 != 0) {
-                            list.get(i).add(case1 / case2);
+                            currentResult.add(case1 / case2);
                         }
                     }
                 }
             }
 
-            if (list.get(i).contains(number)) {
+            if (currentResult.contains(number)) {
                 return i + 1;
             }
         }
 
         return -1;
-    }
-
-    private String stringRepeat(int N, int times) {
-        StringBuilder answer = new StringBuilder();
-        String str = String.valueOf(N);
-        answer.append(str.repeat(Math.max(0, times)));
-        return answer.toString();
     }
 }
