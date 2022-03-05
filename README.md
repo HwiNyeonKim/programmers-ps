a7046c5190ab197301ab36ef651b580d31b0b10a
HwiNyeon Kim <z2b23173us@gmail.com>
2022-03-05 11:05:50 +0900

    [Refactor] - [Graph] 가장 먼 노드
    * 불필요한 연산 제거 및 계산 방식 변경
    * 테스트 케이스 1 ~ 6 : 약간의 효율성 저하
    * 테스트 케이스 7 ~ 9 : 약간의 효율성 향상

--- README.md
+++ README.md
@@ -17,3 +17,4 @@
 | [등굣길](https://programmers.co.kr/learn/courses/30/lessons/42898) | DP | 3 | Solved | [링크](https://praetoriani.tistory.com/29) |
 | [배달](https://programmers.co.kr/learn/courses/30/lessons/12978) | Graph | 2 | Solved | [링크](https://praetoriani.tistory.com/39) |
 | [가장 긴 팰린드롬](https://programmers.co.kr/learn/courses/30/lessons/12904) | Brute Force | 3 | Solved | [링크](https://praetoriani.tistory.com/34) |
+| [가장 먼 노드](https://programmers.co.kr/learn/courses/30/lessons/49189) | Graph | 3 | Solved | [링크](https://praetoriani.tistory.com/30) |
--- src/graph/FarthestNodeTest.java
+++ src/graph/FarthestNodeTest.java
@@ -9,12 +9,38 @@ class FarthestNodeTest {
 
     @Test
     void solution1() {
-        assertEquals(3, obj.solution(6, new int[][] {
+        assertEquals(3, obj.solution(6, new int[][]{
                 {3, 6},
                 {4, 3},
                 {3, 2},
                 {1, 3},
                 {1, 2},
+                {2, 4},
+                {5, 2}
+        }));
+    }
+
+    @Test
+    void solution2() {
+        assertEquals(3, obj.solution(6, new int[][]{
+                {3, 6},
+                {4, 3},
+                {3, 5},
+                {1, 3},
+                {1, 5},
+                {2, 4},
+                {5, 2}
+        }));
+    }
+
+    @Test
+    void solution3() {
+        assertEquals(3, obj.solution(6, new int[][]{
+                {3, 6},
+                {4, 6},
+                {6, 5},
+                {1, 6},
+                {1, 5},
                 {2, 4},
                 {5, 2}
         }));
