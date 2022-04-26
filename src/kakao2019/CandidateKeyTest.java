4cce56cf0859c596e5ac257dc3f2fa4fd27d283e
HwiNyeon Kim <z2b23173us@gmail.com>
2022-04-26 11:48:13 +0900

    [2019 Kakao Blind Recruitment] 후보키

---
+++ src/kakao2019/CandidateKeyTest.java
@@ -0,0 +1,21 @@
+package kakao2019;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CandidateKeyTest {
+    CandidateKey obj = new CandidateKey();
+
+    @Test
+    void solution() {
+        assertEquals(2, obj.solution(new String[][] {
+                {"100","ryan","music","2"},
+                {"200","apeach","math","2"},
+                {"300","tube","computer","3"},
+                {"400","con","computer","4"},
+                {"500","muzi","music","3"},
+                {"600","apeach","music","2"}
+        }));
+    }
+}
