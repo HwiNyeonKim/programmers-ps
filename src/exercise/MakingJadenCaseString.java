5a15799a5f9412a63e994a0ee1fa7ce716eb5a45
HwiNyeon Kim <z2b23173us@gmail.com>
2022-05-07 17:53:18 +0900

    [연습문제] JadenCase 문자열 만들기

---
+++ src/exercise/MakingJadenCaseString.java
@@ -0,0 +1,26 @@
+package exercise;
+
+// URL: https://programmers.co.kr/learn/courses/30/lessons/12951
+// Level: 2
+// 연습문제 - JadenCase 문자열 만들기
+public class MakingJadenCaseString {
+    public String solution(String s) {
+        char[] chars = s.toLowerCase().toCharArray();
+        boolean isFirst = true;
+
+        for (int i = 0; i < chars.length; i++) {
+            if ('0' <= chars[i] && chars[i] <= '9') {
+                isFirst = false;
+            } else if (chars[i] == (char) 32) {
+                isFirst = true;
+            } else {
+                if (isFirst) {
+                    chars[i] = (char) (chars[i] + ('A' - 'a'));
+                    isFirst = false;
+                }
+            }
+        }
+
+        return new String(chars);
+    }
+}
---
+++ src/exercise/MakingJadenCaseStringTest.java
@@ -0,0 +1,19 @@
+package exercise;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MakingJadenCaseStringTest {
+    MakingJadenCaseString obj = new MakingJadenCaseString();
+
+    @Test
+    void solution1() {
+        assertEquals("3people Unfollowed Me", obj.solution("3people unFollowed me"));
+    }
+
+    @Test
+    void solution2() {
+        assertEquals("For The Last Week", obj.solution("for the last week"));
+    }
+}
