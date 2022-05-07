f6cbb824580ba77a14a2d60cf4d7834feccc47f3
HwiNyeon Kim <z2b23173us@gmail.com>
2022-05-07 16:35:59 +0900

    [연습문제] 가장 큰 정사각형 찾기

---
+++ src/exercise/SearchingLargestSquareTest.java
@@ -0,0 +1,34 @@
+package exercise;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SearchingLargestSquareTest {
+    SearchingLargestSquare obj = new SearchingLargestSquare();
+
+    @Test
+    void solution1() {
+        assertEquals(9, obj.solution(new int[][] {
+                {0,1,1,1},
+                {1,1,1,1},
+                {1,1,1,1},
+                {0,0,1,0}
+        }));
+    }
+
+    @Test
+    void solution2() {
+        assertEquals(4, obj.solution(new int[][] {
+                {0,0,1,1},
+                {1,1,1,1}
+        }));
+    }
+
+    @Test
+    void solution3() {
+        assertEquals(1, obj.solution(new int[][] {
+                {0,0,1,1},
+        }));
+    }
+}
